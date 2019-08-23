eb2407d5822469200312ca01be5e5169f95c5ef6
Torchcc <35572470+Torchcc@users.noreply.github.com>
2019-08-23 14:39:00 +0800

    add a hook to run a shell script before starting

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #! /bin/sh
 
+set -e
+
+# If there's a prestart.sh script in the /opt/django/app directory, run it before starting
+PRE_START_PATH=/opt/django/app/prestart.sh
+echo "Checking for script in $PRE_START_PATH"
+if [ -f $PRE_START_PATH ] ; then
+    echo "Running script $PRE_START_PATH"
+    . $PRE_START_PATH
+else
+    echo "There is no script $PRE_START_PATH"
+fi
+
 MODULE=${MODULE:-website}
 
 sed -i "s#module=website.wsgi:application#module=${MODULE}.wsgi:application#g" /opt/django/uwsgi.ini
